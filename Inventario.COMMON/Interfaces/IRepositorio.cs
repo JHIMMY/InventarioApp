30133d6bb7539d4ef812aaa1db8fd638bcaa6b67
Jhimmy <jadsa_corp@hotmail.com>
2022-08-07 23:24:58 -0400

    Repositorio de Empleados terminado

--- Inventario.COMMON/Interfaces/IRepositorio.cs
+++ Inventario.COMMON/Interfaces/IRepositorio.cs
@@ -9,6 +9,6 @@ public interface IRepositorio<T> where T : Base
 {
     bool Create(T entidad);
     List<T> Read { get; }
-    bool Update(string id, T entidadModificada);
-    bool Delete(T entidad);
+    bool Update(T entidadModificada);
+    bool Delete(string id);
 }
--- Inventario.DAL/RepositorioDeEmpleados.cs
+++ Inventario.DAL/RepositorioDeEmpleados.cs
@@ -1,23 +1,106 @@
 ﻿using Inventario.COMMON.Entidades;
 using Inventario.COMMON.Interfaces;
+using LiteDB;
 
 namespace Inventario.DAL;
+
 public class RepositorioDeEmpleados : IRepositorio<Empleado>
 {
-    public List<Empleado> Read => throw new NotImplementedException();
+    private string dbName = "Inventario.db";
+    private string tableName = "Empleados";
 
+    public delegate void CRUDdelegate(LiteDatabase db);
+
+    public List<Empleado> Read
+    {
+        get
+        {
+            List<Empleado> datos = new();
+            using (var db = new LiteDatabase(dbName)) // if already exists then it only opens the db
+            {
+                datos = db.GetCollection<Empleado>(tableName).FindAll().ToList();
+            }
+
+            return datos;
+        }
+    }
+
+    // old full methods for CUD
     public bool Create(Empleado entidad)
     {
-        throw new NotImplementedException();
+        entidad.Id = Guid.NewGuid().ToString(); // guid generates an aleatory 37 digits number
+
+        try
+        {
+            using (var db = new LiteDatabase(dbName))
+            {
+                var coleccion = db.GetCollection<Empleado>(tableName);
+                coleccion.Insert(entidad);
+            }
+
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+    public bool Update(Empleado entidadModificada)
+    {
+        try
+        {
+            using (var db = new LiteDatabase(dbName))
+            {
+                var coleccion = db.GetCollection<Empleado>(tableName);
+                coleccion.Update(entidadModificada);
+            }
+
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
     }
 
-    public bool Delete(Empleado entidad)
+    public bool Delete(string id)
     {
-        throw new NotImplementedException();
+        try
+        {
+            using (var db = new LiteDatabase(dbName))
+            {
+                var coleccion = db.GetCollection<Empleado>(tableName);
+                coleccion.Delete(new BsonValue(id));
+                //r = coleccion.Delete(e => e.Id == id); // this was for many 
+            }
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
     }
 
-    public bool Update(Empleado entidadOriginal, Empleado entidadModificada)
+
+    /// <summary>
+    /// Method that represents Create, Update and Delete operations
+    /// </summary>
+    /// <param name="execute">A delegate to perform the desired task could be any CUD</param>
+    /// <returns></returns>
+    public bool CudOperation(CRUDdelegate execute)
     {
-        throw new NotImplementedException();
+        try
+        {
+            using (var db = new LiteDatabase(dbName))
+            {
+                execute?.Invoke(db);
+            }
+
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
     }
 }
