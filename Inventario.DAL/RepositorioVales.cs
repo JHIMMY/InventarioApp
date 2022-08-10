6dbf119ece837c580707557a9e039fe2345fb1d6
Jhimmy <jadsa_corp@hotmail.com>
2022-08-09 20:21:40 -0400

    Addes the missing repositories for materials and vales

---
+++ Inventario.DAL/RepositorioVales.cs
@@ -0,0 +1,92 @@
+﻿using Inventario.COMMON.Entidades;
+using Inventario.COMMON.Interfaces;
+using LiteDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventario.DAL;
+public class RepositorioVales : IRepositorio<Vale>
+{
+    private string dbName = "Inventario.db";
+    private string tableName = "Empleados";
+
+    public List<Vale> Read
+    {
+        get
+        {
+            List<Vale> datos = new();
+            using(var db = new LiteDatabase(dbName))
+            {
+                datos = db.GetCollection<Vale>(tableName).FindAll().ToList();
+            }
+
+            return datos;
+        }
+    }
+
+    public bool Create(Vale entidad)
+    {
+        entidad.Id = Guid.NewGuid().ToString();
+
+        try
+        {
+
+            using (var db = new LiteDatabase(dbName))
+            {
+                var coleccion = db.GetCollection<Vale>(tableName);
+                coleccion.Insert(entidad);
+            }
+
+            return true;
+           
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+
+    public bool Delete(string id)
+    {
+        try
+        {
+
+            using (var db = new LiteDatabase(dbName))
+            {
+                var coleccion = db.GetCollection<Vale>(tableName);
+                coleccion.Delete(id);
+            }
+
+            return true;
+
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+
+    }
+
+    public bool Update(Vale entidadModificada)
+    {
+        try
+        {
+
+            using (var db = new LiteDatabase(dbName))
+            {
+                var coleccion = db.GetCollection<Vale>(tableName);
+                coleccion.Update(entidadModificada);
+            }
+
+            return true;
+
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+}
---
+++ Inventario.DAL/RepositotioMateriales.cs
@@ -0,0 +1,80 @@
+﻿using Inventario.COMMON.Entidades;
+using Inventario.COMMON.Interfaces;
+using LiteDB;
+
+namespace Inventario.DAL;
+public class RepositotioMateriales : IRepositorio<Material>
+{
+    private string dbName = "Inventario.db";
+    private string tableName = "Materiales";
+
+    public List<Material> Read
+    {
+        get
+        {
+            List < Material > datos = new();
+            using(var db = new LiteDatabase(dbName))
+            {
+                datos = db.GetCollection<Material>(tableName).FindAll().ToList();
+            }
+
+            return datos;
+        }
+    }
+
+    public bool Create(Material entidad)
+    {
+        entidad.Id = Guid.NewGuid().ToString();
+
+        try
+        {
+            using (var db = new LiteDatabase(dbName))
+            {
+                var coleccion = db.GetCollection<Material>(tableName);
+                coleccion.Insert(entidad);
+            }
+
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+
+    public bool Delete(string id)
+    {
+        try
+        {
+            using (var db = new LiteDatabase(dbName))
+            {
+                var coleccion = db.GetCollection<Material>(tableName);
+                coleccion.Delete(id);
+            }
+
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+
+    public bool Update(Material entidadModificada)
+    {
+        try
+        {
+            using (var db = new LiteDatabase(dbName))
+            {
+                var coleccion = db.GetCollection<Material>(tableName);
+                coleccion.Update(entidadModificada);
+            }
+
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+}
