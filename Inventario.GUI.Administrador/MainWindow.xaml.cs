2cbe3f753145422378abf79deb3e0f755293c9fb
Jhimmy <jadsa_corp@hotmail.com>
2022-08-15 22:15:38 -0400

    Interfaz de usuario de Administrado-Empleados Terminada. Falta de materiales y los reportes.

--- Inventario.GUI.Administrador/MainWindow.xaml.cs
+++ Inventario.GUI.Administrador/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Inventario.Biz;
+using Inventario.COMMON.Entidades;
 using Inventario.COMMON.Interfaces;
 using Inventario.DAL;
 using System;
@@ -63,23 +64,92 @@ public partial class MainWindow : Window
 
     private void btnEmpleadosEditar_Click(object sender, RoutedEventArgs e)
     {
+
+        if (dataGridEmpleados.SelectedItem is Empleado empleado)
+        {
+            txtBlockEmpleadosId.Text = empleado.Id;
+            txtBoxEmpleadosNombre.Text = empleado.Nombre;
+            txtBoxEmpleadosApellidos.Text = empleado.Apellidos;
+            txtBoxEmpleadosArea.Text = empleado.Area;
+
+            accionEmpleados = Accion.Editar;
+            PonerBotonesEmpleadosEnEdicion(true);
+        }
+
+
+    }
+
+    private void BtnEmpleadosGuardar_Click(object sender, RoutedEventArgs e)
+    {
+        if (accionEmpleados == Accion.Nuevo)
+        {
+            var empleado = new Empleado()
+            {
+                Nombre = txtBoxEmpleadosNombre.Text,
+                Apellidos = txtBoxEmpleadosApellidos.Text,
+                Area = txtBoxEmpleadosArea.Text,
+            };
+
+            if (manejadorEmpleados.Agregar(empleado))
+            {
+                MessageBox.Show("Empleado agreagado correctamente", "Inventarios", MessageBoxButton.OK, MessageBoxImage.Information);
+                LimpiarCamposDeEmpleados();
+                ActualizarTablaEmpleados();
+                PonerBotonesEmpleadosEnEdicion(false);
+            }
+            else
+            {
+                MessageBox.Show("El empleado no se pudo agregar", "Inventarios", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        else
+        {
+            // editar
+            var empleado = dataGridEmpleados.SelectedItem as Empleado;
+
+            empleado.Nombre = txtBoxEmpleadosNombre.Text;
+            empleado.Apellidos = txtBoxEmpleadosApellidos.Text;
+            empleado.Area = txtBoxEmpleadosArea.Text;
+
+            if (manejadorEmpleados.Modificar(empleado))
+            {
+                MessageBox.Show("Empleado modificado correctamente", "Inventarios", MessageBoxButton.OK, MessageBoxImage.Information);
+                LimpiarCamposDeEmpleados();
+                ActualizarTablaEmpleados();
+                PonerBotonesEmpleadosEnEdicion(false);
+            }
+            else
+            {
+                MessageBox.Show("El empleado no se pudo actualizar", "Inventarios", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+
+    private void BtnEmpleadosCancelar_Click(object sender, RoutedEventArgs e)
+    {
         LimpiarCamposDeEmpleados();
-        PonerBotonesEmpleadosEnEdicion(true);
-        accionEmpleados = Accion.Editar;
+        PonerBotonesEmpleadosEnEdicion(false);
     }
 
-    private void btnEmpleadosGuardar_Click(object sender, RoutedEventArgs e)
+    private void BtnEmpleadosEliminar_Click(object sender, RoutedEventArgs e)
     {
+        if (dataGridEmpleados.SelectedItem is Empleado empleado)
+        {
 
-    }
+            if (MessageBox.Show("Realmente deseas eliminar este empleado?", "Inventarios", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                if (manejadorEmpleados.Eliminar(empleado.Id))
+                {
+                    MessageBox.Show("Empleado eliminado", "Inventarios", MessageBoxButton.OK, MessageBoxImage.Information);
 
-    private void btnEmpleadosCancelar_Click(object sender, RoutedEventArgs e)
-    {
+                    ActualizarTablaEmpleados();
+                }
+                else
+                {
+                    MessageBox.Show("No se pudo elimanar el empleado", "Inventarios", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
 
-    }
-
-    private void btnEmpleadosEliminar_Click(object sender, RoutedEventArgs e)
-    {
-
+            }
+        }
     }
 }
