3e3764bce001293df314280a3c578464e4a0e6b2
Jhimmy <jadsa_corp@hotmail.com>
2022-08-15 20:51:20 -0400

    Populating th e UI with events for Tab Index Empleados for creating new or edit the older ones. Some code behind added and connection to DAL layer is already made.

--- Inventario.GUI.Administrador/MainWindow.xaml.cs
+++ Inventario.GUI.Administrador/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Inventario.Biz;
+using Inventario.COMMON.Interfaces;
+using Inventario.DAL;
+using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Inventario.GUI.Administrador;
 /// <summary>
@@ -19,8 +10,76 @@ namespace Inventario.GUI.Administrador;
 /// </summary>
 public partial class MainWindow : Window
 {
+    enum Accion
+    {
+        Nuevo,
+        Editar
+    }
+
+    private Accion accionEmpleados;
+
+    private readonly IManejadorEmpleados manejadorEmpleados;
+
     public MainWindow()
     {
         InitializeComponent();
+
+        manejadorEmpleados = new ManejadorEmpleados(new RepositorioDeEmpleados());
+
+        PonerBotonesEmpleadosEnEdicion(false);
+        LimpiarCamposDeEmpleados();
+        ActualizarTablaEmpleados();
+    }
+
+    private void ActualizarTablaEmpleados()
+    {
+        //dataGridEmpleados.ItemsSource = null;
+        dataGridEmpleados.ItemsSource = manejadorEmpleados.Listar;
+    }
+
+    private void LimpiarCamposDeEmpleados()
+    {
+        txtBlockEmpleadosId.Text = string.Empty;
+        txtBoxEmpleadosNombre.Clear();
+        txtBoxEmpleadosApellidos.Clear();
+        txtBoxEmpleadosArea.Clear();
+    }
+
+    private void PonerBotonesEmpleadosEnEdicion(bool value)
+    {
+        btnEmpleadosEditar.IsEnabled = !value;
+        btnEmpleadosEliminar.IsEnabled = !value;
+        btnEmpleadosNuevo.IsEnabled = !value;
+        btnEmpleadosGuardar.IsEnabled = value;
+        btnEmpleadosCancelar.IsEnabled = value;
+    }
+
+    private void btnEmpleadosNuevo_Click(object sender, RoutedEventArgs e)
+    {
+        LimpiarCamposDeEmpleados();
+        PonerBotonesEmpleadosEnEdicion(true);
+        accionEmpleados = Accion.Nuevo;
+    }
+
+    private void btnEmpleadosEditar_Click(object sender, RoutedEventArgs e)
+    {
+        LimpiarCamposDeEmpleados();
+        PonerBotonesEmpleadosEnEdicion(true);
+        accionEmpleados = Accion.Editar;
+    }
+
+    private void btnEmpleadosGuardar_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void btnEmpleadosCancelar_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void btnEmpleadosEliminar_Click(object sender, RoutedEventArgs e)
+    {
+
     }
 }
